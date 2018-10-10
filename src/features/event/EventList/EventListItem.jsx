e74e8b67462544417a3d11c168e982c2c90ee7a2
John Hashem <jphyqr@gmail.com>
2018-10-10 15:33:26 -0600

    end of section 6

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
-import EventListAttendee from './EventListAttendee'
+import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
   render() {
-
-    const {event, onEventOpen, deleteEvent} = this.props;
+    const { event, onEventOpen, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
           <Item.Group>
             <Item>
-              <Item.Image
-               size="tiny" 
-               circular 
-               src={event.hostPhotoURL}
-               />
+              <Item.Image size="tiny" circular src={event.hostPhotoURL} />
               <Item.Content>
                 <Item.Header as="a">{event.title}</Item.Header>
                 <Item.Description>
@@ -27,28 +22,34 @@ class EventListItem extends Component {
         </Segment>
         <Segment>
           <span>
-            <Icon name="clock" /> {event.date} |<Icon name="marker" /> {event.venue}
+            <Icon name="clock" /> {event.date} |<Icon name="marker" />{" "}
+            {event.venue}
           </span>
         </Segment>
         <Segment secondary>
           <List horizontal>
-          {event.attendee && event.attendees.map((attendee)=>(
-
-            <EventListAttendee
-            key={attendee.id}
-            attendee={attendee}
-            />
-          )
-
-          )}
-         
-          
+            {event.attendee &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
-        <span>{event.description}</span>
-          <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
-          <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
+          <span>{event.description}</span>
+          <Button
+            onClick={onEventOpen(event)}
+            as="a"
+            color="teal"
+            floated="right"
+            content="View"
+          />
+          <Button
+            onClick={deleteEvent(event.id)}
+            as="a"
+            color="red"
+            floated="right"
+            content="Delete"
+          />
         </Segment>
       </Segment.Group>
     );
